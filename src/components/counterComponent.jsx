353661585f26a63904b391af076b2c30cec526ea
msaaddev <mrsaadirfan@gmail.com>
2019-07-25 21:56:21 +0500

    📦NEW: adding classes dynamically

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -7,21 +7,31 @@ class Counter extends Component {
   };
 
   styles = {
-      color: 'black',
-      fontSize: '16px',
-      backgroundColor: 0x2D2B55,
-  }
-
+    fontSize: "16px",
+    backgroundColor: 0x2d2b55
+  };
 
   render() {
+
     return (
       <div>
-        <span style = {this.styles} className = "badge badge-primary m-2">{this.formatCount()}</span>
-        <button className = "btn btn-secondary btn-sm">Increment</button>
+        <span style={this.styles} className={this.getCountClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
+    getCountClasses() {
+        let counts = "badge m-2 badge-";
+        if (this.state.count === 0)
+            counts += "primary";
+        else
+            counts += "warning";
+        return counts;
+    }
+    
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
