51eef7deac5af7f512989605b3fbce7dbf9f319d
msaaddev <mrsaadirfan@gmail.com>
2019-07-25 21:40:21 +0500

    📦NEW: adding classes and styles

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,14 +3,21 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageURL: "https://picsum.photos/201",
+    imageURL: "https://picsum.photos/201"
   };
+
+  styles = {
+      color: 'black',
+      fontSize: '16px',
+      backgroundColor: 0x2D2B55,
+  }
+
+
   render() {
     return (
       <div>
-        <img src={this.state.imageURL} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style = {this.styles} className = "badge badge-primary m-2">{this.formatCount()}</span>
+        <button className = "btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
