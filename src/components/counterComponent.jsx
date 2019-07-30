44db50dc6bfac0f2b4d6a25fd838b91dc26c7f9e
msaaddev <mrsaadirfan@gmail.com>
2019-07-30 20:15:54 +0500

    📦NEW: delete button

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value
   };
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ value: this.state.value + 1});
-  }
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
@@ -16,7 +16,18 @@ class Counter extends Component {
         <span style={this.styles} className={this.getCountClasses()}>
           {this.formatCount()}
         </span>
-        <button onClick = {() => this.handleIncrement("Incremented")} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => this.handleIncrement("Incremented")}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,17 +4,26 @@ import Counter from "./counterComponent";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 1 },
-      { id: 3, value: 1 },
-      { id: 4, value: 1 }
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
+  };
+
+  counterDelete = (counterId) => {
+    const newArr = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters: newArr})
   };
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            onDelete={this.counterDelete}
+            counter ={counter}
+          />
         ))}
       </div>
     );
