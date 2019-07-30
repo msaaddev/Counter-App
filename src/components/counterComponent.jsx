6fbf2e8dd45f7076107e17875bec20b21b2e7204
msaaddev <mrsaadirfan@gmail.com>
2019-07-30 18:47:46 +0500

    📦NEW: added props

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,27 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageURL: "https://picsum.photos/201",
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value,
   };
-
-
-  styles = {
-    fontSize: "16px",
-    backgroundColor: 0x2d2b55
-  };
-
-
-  // this method is dynamically rendering all the tags in the tags array
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>Please enter tags!</p>;
-    else return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
-  }
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1});
+    this.setState({ value: this.state.value + 1});
   }
 
   render() {
@@ -32,20 +17,19 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button onClick = {() => this.handleIncrement("Incremented")} className="btn btn-secondary btn-sm">Increment</button>
-        {/* <ul>{this.renderTags()}</ul> */}
       </div>
     );
   }
 
   getCountClasses() {
     let counts = "badge m-2 badge-";
-    if (this.state.count === 0) counts += "warning";
+    if (this.state.value === 0) counts += "warning";
     else counts += "primary";
     return counts;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import Counter from './counterComponent';
+import React, { Component } from "react";
+import Counter from "./counterComponent";
 
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 1},
-            {id: 1, value: 1},
-            {id: 1, value: 1},
-            {id: 1, value: 1}]
-     }
-    render() {
-        return ( <div>
-            {this.state.counters.map(counter => <Counter key = {counter.id}/>)}
-        </div> );
-    }
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 1 },
+      { id: 3, value: 1 },
+      { id: 4, value: 1 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Counters;
